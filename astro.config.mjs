82713f12d92afb16b4e54541ebc71cf10b05a01c
shoheiymmt <56075450+shoheiymmt@users.noreply.github.com>
2026-08-21 22:39:45 +0900

    Redirect posts path to homepage

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,12 @@ import { defineConfig } from 'astro/config';
 
 const [owner, repository] = (process.env.GITHUB_REPOSITORY ?? '').split('/');
 const isUserSite = repository?.endsWith('.github.io');
+const base = repository && !isUserSite ? `/${repository}` : '';
 
 export default defineConfig({
   site: owner && repository ? `https://${owner}.github.io` : 'http://localhost:4321',
-  base: repository && !isUserSite ? `/${repository}` : undefined,
+  base: base || undefined,
+  redirects: {
+    '/posts': `${base}/`,
+  },
 });
