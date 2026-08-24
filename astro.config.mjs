d20eacac8dca048fa6f0b63a6e2555c3e1f8f33f
shoheiymmt <56075450+shoheiymmt@users.noreply.github.com>
2026-08-25 02:25:03 +0900

    Configure custom site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,8 @@
 import { defineConfig } from 'astro/config';
 
-const [owner, repository] = (process.env.GITHUB_REPOSITORY ?? '').split('/');
-const isUserSite = repository?.endsWith('.github.io');
-const base = repository && !isUserSite ? `/${repository}` : '';
-
 export default defineConfig({
-  site: owner && repository ? `https://${owner}.github.io` : 'http://localhost:4321',
-  base: base || undefined,
+  site: 'https://shoheiymmt.io',
   redirects: {
-    '/posts': `${base}/`,
+    '/posts': '/',
   },
 });
